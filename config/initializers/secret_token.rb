f4fc2fd38b400b53a1e222193793ee2b9c12464c
Moe <badakhms@gmail.com>
2015-05-24 14:56:35 -0400

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '1de6f8dff0560bb23f79133a64bb790947b3aed2cd77547fe03957112e4f8358e131c905be759e387aa6fee9f95f6f48b2425ce5465b24ec7950484f1728ac1e'
